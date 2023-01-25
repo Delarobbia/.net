72b193723f1e3239dd5fd271ce743296e0ddf094
Victor Changuan <carecarebamerto@gmail.com>
2023-01-25 15:09:28 +0100

    Primer ejercicio

---
+++ University/universityapibackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Universityapibackend.Models.DataModels;
+
+namespace Universityapibackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        {
+
+        }
+   
+        //TODO: Add DbSets (Tables of our Data base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Curso { get; set; }
+    
+    }
+
+    
+
+}
---
+++ University/universityapibackend/Models/DataModels/Curso.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Universityapibackend.Models.DataModels
+{
+    public class Curso : BaseEntity
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [StringLength(280)]
+        public string Descripcion_corta { get; set; }
+
+        public string Descripcion_larga { get; set; }
+
+        public string Publico_objetivo { get; set; }
+
+        public string Objetivos { get; set; }
+
+        public string Requisitos { get; set; }
+
+        public enum Nivel {Basico,Intermedio,Avanzado }
+        
+    }
+}
